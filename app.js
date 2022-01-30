9cb210a23e66f0fd858dfc7da5e8a85da39b9dfb
Jacob <Jacob.McCann214@gmail.com>
2022-01-30 17:40:19 -0600

    Added app.js, connect to index.html

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const navSlide = () => {
     const stack = document.querySelector('.stack');
     const nav = document.querySelector('.nav-links');
-
+    const navLinks = document.querySelectorAll('.nav-links li');
+    /*Toggles navigation*/
     stack.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
+    });
+    /*Animate links*/
+    navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2}s`;
     });
 }
 

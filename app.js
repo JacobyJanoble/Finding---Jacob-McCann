ddea1d275b4fff43c6af8dd88972b5cb10fb063f
Jacob <Jacob.McCann214@gmail.com>
2022-01-31 12:25:34 -0600

    Turned the animated link into an if statement

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const navSlide = () => {
     /*Toggles navigation*/
     stack.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
-    });
+
     /*Animate links*/
     navLinks.forEach((link, index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2}s`;
+        if(link.style.animation) {
+            link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+            }
+        });
     });
 }
+   
 
 navSlide();
